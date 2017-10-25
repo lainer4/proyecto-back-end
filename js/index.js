b8b67c61aa753754cdcbb535ea001fb34e43bb2d
jairo lainer moreno guevara <jmjlainer24@gmail.com>
2017-10-25 13:56:09 -0500

    ciudades
    filtro de ciudades

--- js/index.js
+++ js/index.js
@@ -49,4 +49,58 @@ function playVideoOnScroll(){
 }
 
 inicializarSlider();
-playVideoOnScroll();
+//playVideoOnScroll();
+
+/*
+Funciones de tipo Controlador para la interacción con el back-end
+*/
+function AjaxReq(urlReq, dataSubmit, controlView="wraperInmuebles", load=true, callback){
+    callback = callback || function(){};
+    $.ajax({
+      method: "POST"
+      ,url: urlReq
+      ,data: dataSubmit
+    })
+      .done(function( rsp) {
+        let $control = $( "#" + controlView );
+        if(load){
+          $control.html( rsp );
+        }
+        callback(rsp, $control);
+      })
+      .fail(function( jqXHR, textStatus ) {
+        alert( "Request failed: " + textStatus );
+      });
+}
+
+$(document).ready(()=> {
+
+    var insertOptions = function(rsp, control){ $(control).append(rsp).material_select(); };
+    AjaxReq("back/Ciudades_List.php", {}, "selectCiudad", false, insertOptions);
+    AjaxReq("back/tipos_List.php", {}, "selectTipo", false, insertOptions);
+
+    $("#submitButton").click((event)=>{
+      event.preventDefault();
+      let filtroAply = true;
+      let filtroCiudad = $("#selectCiudad").val();
+      let filtroTipo = $("#selectTipo").val();
+      let filtroPrecioIni = $("#rangoPrecio").val().split(";")[0];
+      let filtroPrecioFin = $("#rangoPrecio").val().split(";")[1];
+      AjaxReq("back/dataShow.php", {fAply: filtroAply
+                                    ,fCiudad: filtroCiudad
+                                    ,fTipo: filtroTipo
+                                    ,fPrecioIni: filtroPrecioIni
+                                    ,fPrecioFin: filtroPrecioFin
+                                  });
+    });
+
+    $("#mostrarTodos").click((event)=>{
+      let filtroPrecioIni = $("#rangoPrecio").val().split(";")[0];
+      let filtroPrecioFin = $("#rangoPrecio").val().split(";")[1];
+      AjaxReq("back/dataShow.php", {
+        fPrecioIni: filtroPrecioIni
+        ,fPrecioFin: filtroPrecioFin
+      });
+    });
+
+  });
